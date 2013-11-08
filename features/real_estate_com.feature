1040c78cb55b24cf0f2ede05d91209350c75883c
Tim Walker <tim.walker@offset1.net>
2013-11-07 17:07:26 -0700

    initial search for property

--- features/real_estate_com.feature
+++ features/real_estate_com.feature
@@ -1,21 +1,24 @@
+@real_estate_com_au
 Feature:
   Using the Cucumber w/Ruby framework complete the following tasks.
   Starting at http://www.realestate.com.au
   i.e. Buy link = realestate.com.au/buy
 
-  Background:
-    Given I am on the home page
+#  Background:
+#    Given I am on the home page
 
+  @nav
   Scenario: Landing Page
     Then all the main navigation links yield expected landing pages
 
+  @wip @search
   Scenario:
     Build a Test that performs a search meeting the following criteria.
     When I search for the following property
-      |State = VIC         |
-      |Suburb = Richmond   |
-      |Property Type = Flat|
-      |Max Price = 500,000 |
+      |state                |       VIC          |
+      |suburb               |       Richmond     |
+      |property type        |       Flat         |
+      |max price            |       500,000      |
     Then all the listings match the State and Suburb
 
 
--- features/step_definitions/real_estate_com_steps.rb
+++ features/step_definitions/real_estate_com_steps.rb
@@ -9,9 +9,9 @@ Capybara.default_driver = :selenium
 Capybara.app_host = 'http://www.realestate.com.au'
 World(Capybara)
 
-Given(/^I am on the home page$/) do
-  page.visit '/'
-end
+#Given(/^I am on the home page$/) do
+#  page.visit '/'
+#end
 
 Then(/^all the main navigation links yield expected landing pages$/) do
   results = page.all('.rui-main-nav a')
@@ -22,9 +22,18 @@ Then(/^all the main navigation links yield expected landing pages$/) do
   verify_links_to_pages(links_to_pages)
 end
 
-When(/^I search for the following property$/) do |table|
-  # table is a Cucumber::Ast::Table
-  pending # express the regexp above with the code you wish you had
+When(/^I search for the following property$/) do |search_table|
+  page.visit '/buy'
+  rh = search_table.rows_hash
+  search_location_criteria = "#{rh[:suburb]};#{rh[:state]}"
+  puts "Searching for properties in #{search_location_criteria}"
+  fill_in('where', :with => search_location_criteria)
+  check('unit apartment')
+  #page.fill_in where
+  #page.find("option[value='20120905']").click
+  #page.find(:css, "#cityID[value='62']").set(true)
+  #page.click searchBtn
+
 end
 
 Then(/^all the listings match the State and Suburb$/) do
@@ -33,6 +42,7 @@ end
 
 def verify_links_to_pages(links_to_verify)
   links_to_verify.each do | text, link |
+    puts "Verify that #{text} is what #{link} refers to"
     page.visit(link)
   end
 end
