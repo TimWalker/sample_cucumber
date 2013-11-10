c0279a8ec334fc813e51a4a006d19fecf8c0ace3
Tim Walker <tim.walker@offset1.net>
2013-11-10 09:56:06 -0700

    basic sendmail working. still bad password

--- python/features/python/sengrid_steps.py
+++ python/features/python/sengrid_steps.py
@@ -13,11 +13,24 @@ def given_i_send_an_email_from_sendgrid(step):
     driver = webdriver.Firefox()
     driver.get('http://sendgrid.com/docs/api_workshop.html')
     driver.switch_to_frame("");
-
     key = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "key")))
     key.send_keys('walketim@gmail.com')
     secret = driver.find_element_by_id('secret')
     secret.send_keys("walketimsp@ssword")
+    toggle = driver.find_element_by_id('toggle-methods')
+    toggle.click()
+    #key = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "params[to]")))
+    param = driver.find_element_by_name('params[to]')
+    param.send_keys("walketim@gmail.com")
+    param = driver.find_element_by_name('params[from]')
+    param.send_keys("walketim@gmail.com")
+    param = driver.find_element_by_name('params[subject]')
+    param.send_keys("Test Mail")
+    param = driver.find_element_by_name('params[text]')
+    param.send_keys("This is a test mail that can be safely discarded by recipient")
+    send_it = driver.find_element_by_id('Mail')
+    send_it.click()
+
     #driver.close()
     assert True, 'This step is a stub'
 
